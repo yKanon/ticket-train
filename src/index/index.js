191994ca98b2ac48232d4d35e8425e7704cbe285
文三思 <394769450@qq.com>
2021-05-30 08:03:07 +0800

    refactor

--- src/index/index.js
+++ src/index/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
-import {} from "react-re";
+import { Provider } from "react-redux";
+
+// In Your Main JS File With Other Imports
+import "./path/to/reseter.css";
 
 import "./index.css";
 import App from "./App";
+import store from "./store";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
